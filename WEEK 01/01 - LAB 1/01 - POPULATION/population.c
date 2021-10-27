1c4400fcaacf1cf88e09997dfc152b6e9f490b56
Gabriela Cleim <71110646+Gabriela-Cleim@users.noreply.github.com>
2021-10-27 16:10:45 -0300

    Add files via upload

---
+++ WEEK 01/01 - LAB 1/01 - POPULATION/population.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size (s)
+    int s;
+    do
+    {
+        s = get_int("Start size: ");
+    }
+    while (s < 9);
+
+    // TODO: Prompt for end size (e)
+    int e;
+    do
+    {
+        e = get_int("End size: ");
+    }
+    while (e < s);
+
+    // TODO: Calculate number of years until we reach threshold
+    int years = 0;
+
+    while (s < e)
+    {
+        s = s + (s / 3) - (s / 4);
+        years++;
+    }
+
+    // TODO: Print number of years
+    printf("Years: %i\n", years);
+}
